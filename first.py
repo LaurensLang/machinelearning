90b8dc8b4d815e101180baf0e8e3f91db77b08c8
Laurens <laurenslang91@gmail.com>
2020-05-20 16:19:30 +0200

    update

--- first.py
+++ first.py
@@ -36,3 +36,6 @@ print(X)
 le = LabelEncoder()
 y = le.fit_transform(y)
 
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
